41ec732a31a9e88d29394ea0911d94894516afdd
刘林亮 <liulinliang@mxtrip.cn>
2016-04-07 15:12:27 +0800

    工厂

---
+++ libs/cache/Cache.php
@@ -0,0 +1,18 @@
+<?php
+
+class Cache {
+
+
+    private $_conf = null;
+
+    public function __construct($conf){
+
+        $this->_conf = $conf;
+    }
+
+    public function init () {
+        $libName = ucfirst($this->_conf['lib_name']);
+        require_once dirname(__FILE__) . "/" . $libName . ".php";
+        return new $libName();
+    }
+}
---
+++ libs/excel/Excel.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Excel {
+
+
+    private $_conf = null;
+
+    public function __construct($conf){
+
+        $this->_conf = $conf;
+    }
+
+    public function init () {
+        $libName = ucfirst($this->_conf['lib_name']);
+        require_once dirname(__FILE__) . "/" . $libName . ".php";
+        return new $libName();
+    }
+}
