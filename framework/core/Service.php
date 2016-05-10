dbbb5488a82cfe1d1f09874f7a39da68148cff4b
liulinlinag <xiaoliang3275@163.com>
2016-05-10 17:24:21 +0800

    update

--- framework/core/Service.php
+++ framework/core/Service.php
@@ -2,6 +2,16 @@
 
 $loader->registerNamespaces([
     'YFF\Framework\Core' => FRAME_ROOT . '/core/',
+    'YFF\Framework\Base' => FRAME_ROOT . '/base/',
+    'YFF\Framework\Base\Download' => FRAME_ROOT . '/base/download/',
+    'YFF\Framework\Base\Filter' => FRAME_ROOT . '/base/filter/',
+    'YFF\Framework\Base\Image' => FRAME_ROOT . '/base/image/',
+    'YFF\Framework\Base\Logger' => FRAME_ROOT . '/base/logger/',
+    'YFF\Framework\Base\Func' => FRAME_ROOT . '/func/',
+    'YFF\Framework\Base\Helper' => FRAME_ROOT . '/helper/',
+    'YFF\Framework\Base\Interface' => FRAME_ROOT . '/interface/',
+    'YFF\Framework\Base\Libs' => FRAME_ROOT . '/libs/',
+    'YFF\Framework\Base\Models' => FRAME_ROOT . '/models/',
 ]);
 
 $loader->register();
